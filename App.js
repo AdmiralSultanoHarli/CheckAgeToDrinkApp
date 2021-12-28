102355f74f69be9a45a27bb2f4d0f7329f9c4911
AdmiralSultano <giordano2010@gmail.com>
2021-12-28 15:47:52 +0700

    Create a validation for checking the age

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import {Text, View, TextInput, Button, Alert} from 'react-native';
+import styles from './styles';
 
 export default function App() {
+
+  const[age, setAge] = useState(0)
+
+  const ageHandler = () => {
+    if(age > 21){
+      Alert.alert("Yap!!", "You can drink")
+    }else{
+      Alert.alert("Oops!!", "You can not drink")
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.input}/>
+      <TextInput style={styles.input} onChangeText={(text)=>setAge(text)} placeholder="Enter your age" idboardType="numeric"/>
+      <TextInput style={styles.input} onChangeText={(text)=>setAge(text)} placeholder="Enter your age" idboardType="numeric" />
+      <View style={styles.button_style}>
+        <Button title="Can i drink?" onPress={()=>ageHandler()} />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      marginTop: 40,
+      padding: 20,
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+    input: {
+      backgroundColor: "#2AADEC",
+      padding: 20,
+      borderWidth: 2,
+      marginTop: 10
+    },
+    button_style: {
+        marginTop: 20
+    }
+  });
+
+export default styles;
